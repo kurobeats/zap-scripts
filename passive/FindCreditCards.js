d057605765955539c05430803abe2b1b298823bf
Anthony Cozamanis <antcoz@outlook.com>
2026-07-15 20:27:22 +0800

    credit card regex update

--- passive/FindCreditCards.js
+++ passive/FindCreditCards.js
@@ -1,4 +1,4 @@
-// Lazily crafted by Anthony Cozamanis - kurobeats@yahoo.co.jp
+// Lazily improved by Anthony Cozamanis - kurobeats@yahoo.co.jp
 // CreditCard Finder by freakyclown@gmail.com
 
 function scan(ps, msg, src)
@@ -31,12 +31,12 @@ function scan(ps, msg, src)
                       found.toString(), alertSolution, '', 0, 0, msg)
     }
 
-    const re_visa = /(^4[0-9]{12}(?:[0-9]{3})?$)/g
-    const re_master = /(5[1-5][0-9]{14})/g
-    const re_amex = /(3[47][0-9]{13})/g
-    const re_disc = /(6(?:011|5[0-9]{2})[0-9]{12})/g
-    const re_diner = /(3(?:0[0-5]|[68][0-9])[0-9]{11})/g
-    const re_jcb = /(^(?:2131|1800|35\d{3})\d{11}$)/g
+    const re_visa = /(\b4[0-9]{12}(?:[0-9]{3})?\b)/g
+    const re_master = /(\b(?:5[1-5][0-9]{14}|2(?:22[1-9]|2[3-9]\d|[3-6]\d{2}|7[0-2]\d|720)\d{12})\b)/g
+    const re_amex = /(\b3[47][0-9]{13}\b)/g
+    const re_disc = /(\b6(?:011|5[0-9]{2}|4[4-9]\d)[0-9]{12}\b)/g
+    const re_diner = /(\b3(?:0[0-5]|[68][0-9])[0-9]{11}\b)/g
+    const re_jcb = /(\b(?:2131|1800|35\d{3})\d{11}\b)/g
 
     const cards = [re_visa, re_master, re_amex, re_disc, re_diner, re_jcb]
     for (let i = 0; i < cards.length; i++) {
